93f198c0f9640059b5f4bf56551def1b8522142c
cnnoka <cnnoka1@gmail.com>
2023-10-19 12:18:34 -0700

    update to 0. What's my name

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,4 +1,5 @@
-0 - print_name.c
+0-print_name.c
+
 
 #include "function_pointers.h"
 #include <stdio.h>
@@ -12,5 +13,7 @@ void print_name(char *name, void (*f)(char *))
 {
 if (name == NULL || f == NULL)
 return;
+
+
 f(name);
 }
